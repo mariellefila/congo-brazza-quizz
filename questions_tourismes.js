03baa72451f192d6ccd0f4a9f8f8c251c2209f66
marielle <mbruchet@live.fr>
2025-07-26 15:09:15 +0200

    ajout de la catégorie touriste avec système photo

---
+++ questions_tourismes.js
@@ -0,0 +1,120 @@
+[
+  {
+    "question": "Quel monument est représenté sur l’image ?",
+    "image": "images/MémorialPierreSavorgnandeBrazza.jpeg",
+    "options": ["Mémorial Pierre Savorgnan de Brazza", "Tour Nabemba", "Basilique Sainte-Anne", "Marché Poto-Poto"],
+    "answer": "Mémorial Pierre Savorgnan de Brazza"
+  },
+  {
+    "question": "Quel site naturel spectaculaire illustré sur l’image représente des gorges près de Pointe-Noire ?",
+    "image": "images/gorgediosso.jpeg",
+    "options": ["Gorges de Diosso", "Chutes de Loufoulakari", "Parc de Nouabalé-Ndoki", "Baie de Loango"],
+    "answer": "Gorges de Diosso"
+  },
+  {
+    "question": "Quel édifice religieux figure sur l’image ?",
+    "image": "images/eglise_notredame_pnr.jpg",
+    "options": ["Église Catholique Notre Dame de L'Assomption", "Cathédrale du Sacré de Brazzaville", "Basilique Sainte-Anne", "Église Saint Michel de Dolisie"],
+    "answer": "Église Catholique Notre Dame de L'Assomption"
+  },
+  {
+    "question": "Quelle ville est connue pour cette cathédrale ?",
+	"image": "images/eglise_notredame_pnr.jpg",
+    "options": ["Brazzaville", "Pointe-Noire", "Dolisie", "Nkayi"],
+    "answer": "Pointe-Noire"
+  },
+  {
+    "question": "Quelle tour emblématique est montrée sur l’image ?",
+    "image": "images/Tour_Nabemba.jpeg",
+    "options": ["Tour Nabemba", "Palais du Peuple", "Musée National du Congo", "Tour de Dolisie"],
+    "answer": "Tour Nabemba"
+  },
+  {
+    "question": "Dans quelle ville se trouve cette tour ?",
+	"image": "images/Tour_Nabemba.jpeg",
+    "options": ["Brazzaville", "Pointe-Noire", "Dolisie", "Loango"],
+    "answer": "Brazzaville"
+  },
+  {
+    "question": "Quel palais présidentiel est visible sur l’image ?",
+	"image": "images/palais_peuple.jpeg",
+    "options": ["Palais du Peuple", "Palais présidentiel de Pointe-Noire", "Palais de Justice", "Palais des Congrès"],
+    "answer": "Palais du Peuple"
+  },
+  {
+    "question": "Quel quartier animé est représenté sur l’image ?",
+	"image": "images/marche_potopoto.jpg",
+    "options": ["Marché Poto-Poto", "Corniche du fleuve Congo", "Marché de Dolisie", "Centre-ville de Pointe-Noire"],
+    "answer": "Marché Poto-Poto"
+  },
+  {
+    "question": "Quel site naturel près de Brazzaville est célèbre pour ses cascades et forêts ?",
+	"image": "images/chutes-loufoulakari-palmier.jpeg",
+    "options": ["Chutes de Loufoulakari", "Gorges de Diosso", "Parc Mbudi", "Odzala-Kokoua"],
+    "answer": "Chutes de Loufoulakari"
+  },
+  {
+    "question": "Quelle école artistique pionnière se trouve à Brazzaville ?",
+	"image": "images/ecole_poto_poto.jpg",
+    "options": ["École de Peinture de Poto-Poto", "Académie des Beaux-Arts", "Centre Culturel Moungali", "Ecole du Plateau"],
+    "answer": "École de Peinture de Poto-Poto"
+  },
+  {
+    "question": "Quel musée de Pointe-Noire est consacré à l’art et l’histoire locale ?",
+	"image": "images/musee_cercle_africain.jpg",
+    "options": ["Musée du Cercle Africain", "Musée Ma-Loango", "Musée National du Congo", "Musée de Dolisie"],
+    "answer": "Musée du Cercle Africain"
+  },
+  {
+    "question": "Quelle baie historique au sud de Pointe-Noire était un ancien port d’esclavage ?",
+	"image": "images/port_Loango.jpg",
+    "options": ["Baie de Loango", "Baie de Tchinouka", "Côte Sauvage", "Baie de Dolisie"],
+    "answer": "Ancien port d’embarquement des esclaves de Loango"
+  },
+  {
+    "question": "Quel parc national près de Pointe-Noire est riche en biodiversité ?",
+	"image": "images/Parc_National_Conkouati-Douli.jpeg",
+    "options": ["Parc National de Conkouati-Douli", "Odzala-Kokoua", "Parc Mbudi", "Forêt du Mayombe"],
+    "answer": "Parc National de Conkouati-Douli"
+  },
+  {
+    "question": "Quel spot naturel côtier sauvage est apprécié près de Pointe-Noire ?",
+	"image": "images/cote_sauvage_pointe_noire.jpg",
+    "options": ["Côte Sauvage", "Plage Sportive", "Baie de Loango", "Baie de Tchinouka"],
+    "answer": "Côte Sauvage"
+  },
+  {
+	  "question": "Quel monument naturel emblématique se trouve au cœur de la ville de Dolisie ?",
+	  "image": "images/arbre_de_dolisie.jpeg",
+	  "options": ["L’arbre de Dolisie", "Le Rocher de Sibiti", "Le Baobab de Nkayi", "Les Gorges de Diosso"],
+	  "answer": "L’arbre de Dolisie"
+  },
+  {
+    "question": "Quel symbole religieux se dresse à Dolisie dans l’enceinte de la ville ?",
+	"image": "images/dolisie-eglise-saint-paul.jpeg",
+    "options": ["Église Saint-Paul", "Cathédrale du Sacré-Cœur", "Basilique Sainte-Anne", "Cathédrale Saint-Pierre"],
+    "answer": "Église Saint-Paul"
+  },
+  {
+	  "question": Où se trouve le site touristique représenté sur cette photo, célèbre pour ses bungalows, sa piscine et sa vue sur l’océan Atlantique ?",
+	  "image": "images/malonda_lodge.jpg",
+	  "options": [
+		"Brazzaville",
+		"Pointe-Noire",
+		"Nkayi",
+		"Owando"
+	  ],
+	  "answer": "Pointe-Noire"
+	},
+	{
+	  "question": "La gare centrale de Pointe-Noire, au Congo-Brazzaville, a été construite dans un style qui rappelle fortement une gare française. Laquelle ?",
+	  "image": "images/gare_centrale_pnr.jpeg",
+	  "options": [
+		"Gare de Lyon (Paris)",
+		"Gare de Deauville",
+		"Gare de La Rochelle",
+		"Gare de Trouville-Deauville"
+	  ],
+	  "answer": "Gare de Trouville-Deauville"
+	}
+]
